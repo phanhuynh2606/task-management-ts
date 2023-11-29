99c77b527e3f74ebfbbdad33abb6c090077e91ad
phanhuynh2606 <huynhpvhe170150@fpt.edu.vn>
2023-11-29 10:31:51 +0700

    Phân Trang

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from "express";
 import Task from "../models/task.model";
+import { paginationHelper } from "../../../helper/pagination";
 
-export const index =  async (req: Request, res: Response) => {
+export const index = async (req: Request, res: Response) => {
   // FIND
   interface Find {
     deleted: boolean;
@@ -15,29 +16,49 @@ export const index =  async (req: Request, res: Response) => {
   }
   // END FIND
 
+  // Pagination
+  const countTasks = await Task.countDocuments(find);
+
+  let objectPagination = paginationHelper(
+    {
+      currentPage: 1,
+      limitItems: 2,
+    },
+    req.query,
+    countTasks
+  );
+
+  //End Pagination
+
   // Sort
   const sort = {};
-  if(req.query.sortKey && req.query.sortValue){
+  if (req.query.sortKey && req.query.sortValue) {
     const sortKey = req.query.sortKey.toString();
     sort[sortKey] = req.query.sortValue;
   }
   // End Sort
-  const tasks = await Task.find(find).sort({});
-  res.json(tasks);
+  const tasks = await Task.find(find)
+  .sort(sort)
+  .limit(objectPagination.limitItems)
+  .skip(objectPagination.skip);
+  res.json({
+    tasks: tasks,
+    total: countTasks,
+  });
 };
 
-export const detail =  async (req: Request, res: Response) => {
-   try {
-     const id: string = req.params.id;
-     const task = await Task.findOne({
-       _id: id,
-       deleted: false,
-     });
-     res.json(task);
-   } catch (error) {
-     res.json({
-       code: 404,
-       message: "Lỗi",
-     });
-   }
- };
+export const detail = async (req: Request, res: Response) => {
+  try {
+    const id: string = req.params.id;
+    const task = await Task.findOne({
+      _id: id,
+      deleted: false,
+    });
+    res.json(task);
+  } catch (error) {
+    res.json({
+      code: 404,
+      message: "Lỗi",
+    });
+  }
+};
