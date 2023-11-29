fc5d48d6fda6971cf916f620f3a586a6cd9850fa
phanhuynh2606 <huynhpvhe170150@fpt.edu.vn>
2023-11-29 10:03:30 +0700

    Sắp xếp theo tiêu chí

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -2,18 +2,27 @@ import { Request, Response } from "express";
 import Task from "../models/task.model";
 
 export const index =  async (req: Request, res: Response) => {
+  // FIND
   interface Find {
     deleted: boolean;
-    status?:string
+    status?: string;
   }
-  const find:Find = {
+  const find: Find = {
     deleted: false,
   };
-  if (req.query.status){
+  if (req.query.status) {
     find.status = req.query.status.toString();
-  } 
+  }
+  // END FIND
 
-  const tasks = await Task.find({});
+  // Sort
+  const sort = {};
+  if(req.query.sortKey && req.query.sortValue){
+    const sortKey = req.query.sortKey.toString();
+    sort[sortKey] = req.query.sortValue;
+  }
+  // End Sort
+  const tasks = await Task.find(find).sort({});
   res.json(tasks);
 };
 
