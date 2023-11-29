363fa098a8224d56029e8b6c179fd735e551bfd0
phanhuynh2606 <huynhpvhe170150@fpt.edu.vn>
2023-11-29 11:26:42 +0700

    Thêm mới 1 công việc

--- api/v1/routes/task.route.ts
+++ api/v1/routes/task.route.ts
@@ -11,4 +11,6 @@ router.get("/detail/:id",controller.detail);
 router.patch("/change-status/:id",controller.changeStatus);
 
 router.patch("/change-multi",controller.changeMulti);
+
+router.post("/create",controller.create);
 export const taskRoutes:Router = router;
