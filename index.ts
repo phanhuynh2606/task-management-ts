36a1452901166d058f5bf8c6475d1dc65c009883
phanhuynh2606 <huynhpvhe170150@fpt.edu.vn>
2023-11-29 12:11:48 +0700

    Cài đặt CORS

--- index.ts
+++ index.ts
@@ -3,11 +3,19 @@ import dotenv from "dotenv";
 import * as database from "./config/database"
 import mainV1Routes from "./api/v1/routes/index.route";
 import bodyParser from"body-parser";
+import cors from "cors"
 dotenv.config();
 database.connect();
 const app:Express = express();
 const port:string | number = process.env.PORT || 3000;
 
+// var corsOptions = {
+//   origin: "http://example.com",
+//   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+// };
+// app.use(cors(corsOptions));
+app.use(cors());
+ 
 // parse application/json
 app.use(bodyParser.json())
 
